478491018b405d11ec6e192098a974bf3d761ce5
Andrew Lee <andrew-lee-zuora@users.noreply.github.com>
2020-06-04 22:23:29 -0700

    Set __init__.__all__

--- zsec_aws_tools_extensions/__init__.py
+++ zsec_aws_tools_extensions/__init__.py
@@ -5,3 +5,10 @@ from .deployment import (
     PartialResourceCollection,
     PartialAWSResourceCollection,
 )
+
+__all__ = ['zip_string',
+           'PartialResource',
+           'partial_resources',
+           'PartialResourceCollection',
+           'PartialAWSResourceCollection',
+           ]
